3308f3ce07a4abd3a0b7e19e564b4d1fd7bc840c
Stephen Mann <thanthese@gmail.com>
2010-12-18 14:04:28 -0600

    Refactored, added final testing.

--- mergesort.scm
+++ mergesort.scm
@@ -5,7 +5,7 @@
 ; Mergesort, implemented in scheme from basic, basic primitives.
 ;
 
-; aliases to consts
+; aliases for consts
 (define true #t)
 (define false #f)
 (define else #t)
@@ -21,9 +21,24 @@
 
 ; null-safe tail
 (define (tail ls)
-  (if (< (length ls) 1)
+  (if (null? ls)
     '()
     (cdr ls)))
+
+; split list into equal parts, return as 2-n list; order is destroyed
+;
+; It does this in a bizarre way. Imagine taking a deck of cards, pulling
+; the cards off the top one at a time, and alternating putting them in
+; two piles. This is what this function does. Therefore, you end up with
+; two, alternating, reversed lists.
+(define (split ls)
+  (define (put-card pile-a pile-b deck toggle)
+    (if (null? deck)
+      (list pile-a pile-b)
+      (if toggle
+        (put-card (cons (head deck) pile-a) pile-b (tail deck) (not toggle))
+        (put-card pile-a (cons (head deck) pile-b) (tail deck) (not toggle)))))
+  (put-card '() '() ls true))
 
 ; Return true if the head of the smaller list is smaller than or equal
 ; to the head of the second list. Null-safe. Consider '() to be smaller
@@ -35,61 +50,38 @@
           ((null? b) false)
           (else (<= a b)))))
 
-; return largest list, based on definition in "first-is-smaller"
-(define (max-list list-a list-b)
-  (if (first-is-smaller list-a list-b)
-    list-b
-    list-a))
-
-; return smallest list, based on definition in "first-is-smaller"
+; return smaller list, based on definition in "first-is-smaller"
 (define (min-list list-a list-b)
   (if (first-is-smaller list-a list-b)
     list-a
     list-b))
 
-; merge two sorted lists into a single, large sorted list
+; return larger list, based on definition in "first-is-smaller"
+(define (max-list list-a list-b)
+  (if (first-is-smaller list-a list-b)
+    list-b
+    list-a))
+
+; merge two sorted lists into a single sorted list
 (define (merge list-a list-b)
   (let ((small (min-list list-a list-b))
         (large (max-list list-a list-b)))
     (if (null? small)
       large
       (cons (head small)
-            (merge large (tail small))))))
+            (merge (tail small) large)))))
 
-; split list into equal parts, return as 2-n list; order is destroyed
-;
-; It does this in a bizarre way. Imagine taking a deck of cards, pulling
-; the cards off the top one at a time, and alternating putting them in
-; two piles. This is what this function does. Therefore, you end up with
-; two, alternating, reversed lists.
-(define (split ls)
-  (define (helper list-a list-b rest toggle)
-    (if (null? (head rest))
-      (list list-a list-b)
-      (if toggle
-        (helper (cons (head rest) list-a) list-b (tail rest) (not toggle))
-        (helper list-a (cons (head rest) list-b) (tail rest) (not toggle)))))
-  (helper '() '() ls true))
+; return if the given list is shorter than two elements
+(define (shorter-than-two? ls)
+  (null? (head (tail ls))))
 
 ; at last, a mergesort
 (define (mergesort ls)
-  (if (< (length ls) 2)
+  (if (shorter-than-two? ls)
     ls
     (let ((pair (split ls)))
       (merge (mergesort (head pair))
              (mergesort (second pair))))))
-
-; the proof
-(mergesort '(3 9 8 1 6 4 2 7 5))
-(mergesort '(3 9 8 3 1 6 3 4 2 3 7 5))
-(mergesort '())
-(mergesort '(2 1))
-
-
-;; RANDOM UTILITIES
-;
-; Things I didn't end up needing, but which are interesting nonetheless.
-;
 
 ; null-safe equals for comparing two scalars
 (define (equals? a b)
@@ -99,8 +91,8 @@
 
 ; null-safe equals for comparing two lists
 (define (equal-lists? list-a list-b)
-  (cond ((and (null? (head list-a))
-              (null? (head list-b)))
+  (cond ((and (null? list-a)
+              (null? list-b))
          true)
         ((equals? (head list-a)
                   (head list-b))
@@ -108,5 +100,10 @@
                        (tail list-b)))
         (else false)))
 
-(equal-lists? '(1 2 3 4 5) (merge '(2 4) '(1 3 5)))
-(equal-lists? '(1 1 3 4 5) (merge '(1 4) '(1 3 5)))
+; the proof
+(list
+  (equal-lists? '(1 2 3 4 5 6 7 8 9) (mergesort '(3 9 8 1 6 4 2 7 5)))
+  (equal-lists? '() (mergesort '()))
+  (equal-lists? '(1 2 3 3 3 4 5 6 7 8 9) (mergesort '(3 9 8 3 1 6  4 2 3 7 5)))
+  (equal-lists? '() (mergesort '()))
+  (equal-lists? '(1 2) (mergesort '(2 1))))
